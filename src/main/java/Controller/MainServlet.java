1bcd89adef4e8506e2d1e0072fc2a6dfb49e85bd
Your Name <max@example.com>
2015-09-15 11:25:54 +0300

    secc

--- src/main/java/Controller/MainServlet.java
+++ src/main/java/Controller/MainServlet.java
@@ -9,28 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
-
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import org.apache.commons.io.IOUtils;
-import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONTokener;
-import org.json.simple.JSONObject;
+
 
 @WebServlet("/")
 public class MainServlet extends HttpServlet {
@@ -39,15 +22,6 @@ public class MainServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException{
         String link1 = "http://servis.e-otg.com/wp-json/posts?filter[name]=%D0%B1%D0%BB%D0%BE%D0%B33"; //just a string
-/*
-        URL url = new URL(link);
-        HttpURLConnection request = (HttpURLConnection) url.openConnection();
-        request.connect();
-*/
-
-       // String link = URLEncoder.encode(link1, "UTF-8");
-
-        // read from the URL
         BufferedReader reader = null;
         try {
             URL url = new URL(link1);
@@ -76,8 +50,6 @@ public class MainServlet extends HttpServlet {
             if (reader != null)
                 reader.close();
         }
-
-
         req.getRequestDispatcher("home.jsp").forward(req, resp);
 
     }
